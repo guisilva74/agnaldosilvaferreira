2beef902ee51a0885396e886c49a7bfe383113cb
guisilva74 <agnaldosilva@sapo.pt>
2023-07-06 11:00:52 +0000

    subir site para cima

---
+++ cad.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado</title>
+    <link rel="stylesheet"href="index.css">
+</head>
+<body>
+    
+<header>Resultado do processamento</header>
+<main>
+
+
+
+<?php
+
+
+$n = $_GET["nome"];
+$s = $_GET["sobrenome"];
+echo "<p>É um grande prazer de conhecer, <strong>$n <strong>$s! Este é o meu ite";
+
+
+    
+?>
+<p><a href="javascript:history. go(-1)">voltar para a página anterior<a></p>
+
+
+
+</main>
+</body>
+</html>
+
